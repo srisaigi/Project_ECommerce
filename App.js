21896db231ca4479304f6ccac3fe865152f49f47
srisaigi <147787845+srisaigi@users.noreply.github.com>
2025-10-09 02:07:03 +0530

    Initialize App component with routing and layout

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <div className="container mt-3 mb-5">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
