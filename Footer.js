e7b0189f781e44754bf592661fee65a6bb0d6f87
srisaigi <147787845+srisaigi@users.noreply.github.com>
2025-10-09 02:08:19 +0530

    Add Footer component for project

---
+++ Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-dark text-white text-center py-3">
+      <p>© 2025 Project E-Commerce. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
