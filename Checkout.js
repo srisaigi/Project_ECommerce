c6bbacab9b849732dd106e3a216ff13767d53dd6
srisaigi <147787845+srisaigi@users.noreply.github.com>
2025-10-09 02:09:59 +0530

    Add Checkout component for order completion

---
+++ Checkout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Checkout() {
+  return (
+    <div>
+      <h2>Checkout</h2>
+      <p>Complete your order securely!</p>
+    </div>
+  );
+}
+
+export default Checkout;
