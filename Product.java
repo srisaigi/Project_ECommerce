61814c05db3da2f9c868c72d6f0e49daded77a76
srisaigi <147787845+srisaigi@users.noreply.github.com>
2025-10-09 01:54:39 +0530

    Add Product entity with JPA annotations

---
+++ Product.java
@@ -0,0 +1,22 @@
+package com.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private double price;
+    private String imageUrl;
+    private String category;
+    private int stock;
+}
