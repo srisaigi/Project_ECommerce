306d8cb237ada36e91ac0824704377da51a7e79a
srisaigi <147787845+srisaigi@users.noreply.github.com>
2025-10-09 01:56:11 +0530

    Add CartItem entity for ecommerce model

---
+++ CartItem.java
@@ -0,0 +1,20 @@
+package com.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Product product;
+
+    private int quantity;
+}
