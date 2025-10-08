109dd36b196344f92af65999f6e05b11026c6fba
srisaigi <147787845+srisaigi@users.noreply.github.com>
2025-10-09 01:55:28 +0530

    Add User entity with JPA annotations

---
+++ User.java
@@ -0,0 +1,21 @@
+package com.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+    private String role; // CUSTOMER / ADMIN
+}
