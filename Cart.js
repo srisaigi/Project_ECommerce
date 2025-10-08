b0f83036d416244d7b0800ada0ba4ad73e9acc7a
srisaigi <147787845+srisaigi@users.noreply.github.com>
2025-10-09 02:09:25 +0530

    Add Cart component with placeholder content

---
+++ Cart.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Cart() {
+  return (
+    <div>
+      <h2>Your Cart</h2>
+      <p>Cart functionality coming soon...</p>
+    </div>
+  );
+}
+
+export default Cart;
